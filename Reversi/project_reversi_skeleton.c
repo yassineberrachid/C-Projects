bec641bb23174c7762a3751c9a7ef7aa4cc46604
yassineberrachid <95399226+yassineberrachid@users.noreply.github.com>
2022-01-05 13:37:30 +0100

    Add files via upload

---
+++ Reversi/project_reversi_skeleton.c
@@ -0,0 +1,78 @@
+/*
+ * File:        project_reversi_skeleton.c
+ * Author:      APS105H1 Teaching Team
+ * Modified by: * You Name Here *
+ *
+ * Date: Jan 2021
+ */
+
+#include "project_reversi_skeleton.h" // DO NOT modify this line
+
+// Note: You may want to add more function declarations here
+// =========   Function declaration  ==========
+/* None for now */
+
+
+// ========= Function implementation ==========
+/*
+ * Function:  printBoard 
+ * --------------------
+ * Print the whole board to the terminal window.
+ */
+void printBoard(char board[][26], int n) {
+    (void)board;
+    (void)n;
+}
+
+/*
+ * Function:  positionInBounds 
+ * --------------------
+ * Checks whether the specified (row, col) lies within the board dimensions.
+ */
+bool positionInBounds(int n, int row, int col) {
+    (void)n;
+    (void)row;
+    (void)col;
+    return false;  
+}
+
+/*
+ * Function:  checkLegalInDirection 
+ * --------------------
+ * Checks whether (row, col) is a legal position for a tile of colour by “looking” in the direction 
+ * specified by deltaRow and deltaCol.
+ */
+bool checkLegalInDirection(char board[][26], int n, int row,
+ 		int col, char colour, int deltaRow, int deltaCol) {
+    (void)board;
+    (void)n;
+    (void)row;
+    (void)col;
+    (void)colour;
+    (void)deltaRow;
+    (void)deltaCol;
+    return false;             
+}
+
+/*
+ * Function:  makeMove 
+ * --------------------
+ * The player "turn" makes a move at (row, col).
+ * Note: This function MUST NOT change board, only row and col can be changed to reflect the move.
+ */
+int makeMove(const char board[26][26], int n, char turn, int *row, int *col) {
+    (void)board;
+    (void)n;
+    (void)turn;
+    (void)row;
+    (void)col;
+    return 0;
+}
+
+#ifndef COMPETE_MODE // DO NOT DELETE THIS LINE
+int main(void) {
+    // Complete your main function here
+
+    return 0;
+}
+#endif // DO NOT DELETE THIS LINE
